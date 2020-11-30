7929623aba9eb6a94309c2936772f8c41b2ac4e5
Hannah Malm <hannahemalm616@gmail.com>
2020-11-30 23:54:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,15 @@
-def hello(array)
+def collect(collection)
   i = 0
-  collection = [] #add in a blank collection array
+  collection = [] 
   while i < array.length
-    collection << #this allows you to insert array items to the end of the colelction. Push the return value of using yield(array[i]) into the collection array
+    collection << 
     yield(array[i])
     i += 1
   end
   collection #here we are returning the collection
 end
 
-#this builds an iteration using a while loop, yielded each individual member of an array to a block and captured the return values of yielding those items to the block
-#the return value of a while loop is always nil. If you want the method to return something else, you have to tell it to do something
+
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
