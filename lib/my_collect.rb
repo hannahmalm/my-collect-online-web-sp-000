dc03d901495f00f088e136110505741226f5cf92
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:11:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,6 +3,7 @@ def my_collect(array)
   #my_collect can handle an empty collection 
   collection = [] 
   while i < array.length
+  #my_collect returns a new collection of appropriately motidifed elements
     # collection << 
     #yields the correct element from the collection
     yield(array[i])
