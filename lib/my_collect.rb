22c07feaba5878a8910820b9147545ecb01eb374
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:11:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
   collection = [] 
   while i < array.length
   #my_collect returns a new collection of appropriately motidifed elements
-    # collection << 
+    collection << 
     #yields the correct element from the collection
     yield(array[i])
     i += 1
